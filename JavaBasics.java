aa5d9f83ff7e15d09e38daece35eb19deabc0957
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2023-09-10 17:32:19 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,11 @@
+public class JavaBasics{
+    public static void main(String args[]){
+        
+        //java code
+        System.out.println("Hello Shaurya!");
+        
+
+    }
+}
+
+//boilerplate code (template)
