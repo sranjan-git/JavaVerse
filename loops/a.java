00eb4384331e0c14abd0a371eec4da4107f19c31
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2023-09-21 00:02:07 +0530

    Add files via upload

---
+++ loops/a.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class a{
+    public static void main(String args[]){
+        int counter = 0;
+        while(counter<100){
+            System.out.println("Hello World!");
+            counter++;
+            
+        }
+        System.out.println("We printed HW 100 times.");
+    }
+    
+}
