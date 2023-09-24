6d4130ccae8a70484ce624225e10e74057c0798f
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2023-09-25 01:17:54 +0530

    Add files via upload

---
+++ loops/b.java
@@ -0,0 +1,14 @@
+// print numbers form 1 to 10
+import java.util.*;
+
+public class b{
+    public static void main(String args[]){
+        int counter = 1;
+        while(counter<=11){
+            System.out.println(counter);
+            counter++;
+            
+        }
+        
+    }
+} 
---
+++ loops/c.java
@@ -0,0 +1,16 @@
+//  pinting numbers form 1 to n
+
+import java.util.*;
+
+public class c{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int range = sc.nextInt();
+        int counter = 1;
+
+        while(counter <= range){
+            System.out.print(counter + " ");
+        }
+        System.out.println();
+    }
+}
