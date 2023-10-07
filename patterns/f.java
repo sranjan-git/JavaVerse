b841dd439010c0245d9080f3b75280b1a96ae3d1
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2023-10-08 02:56:33 +0530

    Add files via upload

---
+++ patterns/f.java
@@ -0,0 +1,13 @@
+// inverted half pyramid with numbers
+package patterns;
+public class f {
+   public static void main(String[] args) {
+       int n = 5;
+       for (int i = n; i >= 1; i--) {
+           for (int j = 1; j <= i; j++) {
+               System.out.print(j + " ");
+           }
+           System.out.println();
+       }
+   }
+}
---
+++ patterns/g.java
@@ -0,0 +1,25 @@
+// print sum using functions
+
+import java.util.*;
+public class g{
+
+    public static void CalculateSum(){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("Sum is: " +sum); // Changed print to println
+    }   
+
+    public static void main (String args[]){
+        CalculateSum();
+
+    }
+
+}
+
+
+
+
+
+    
