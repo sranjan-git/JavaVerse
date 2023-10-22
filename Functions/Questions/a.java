ab6f795eabd37273d10e183e4ab825cae5bcefde
Sudhanshu Ranjan <86006320+sranjan-git@users.noreply.github.com>
2023-10-22 23:49:11 +0530

    Create a.java

---
+++ Functions/Questions/a.java
@@ -0,0 +1,10 @@
+
+public class a {
+    public static void main(String[] args) {
+        System.out.println("The average of 3, 5, and 7 is: " + average(3, 5, 7));
+    }
+
+    public static double average(int num1, int num2, int num3) {
+        return (num1 + num2 + num3) / 3.0;
+    }
+}
